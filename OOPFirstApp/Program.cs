38ef39e53921d86739321a7f8c104f66888de173
Infarh <shmachilin@yandex.ru>
2020-09-22 10:27:46 +0300

    Операторы в классе комплексных чисел

--- OOPFirstApp/Program.cs
+++ OOPFirstApp/Program.cs
@@ -9,17 +9,25 @@ namespace OOPFirstApp
         static void Main(string[] args)
         {
             //StartTime = DateTime.Now;
-
             //Arrays.Start();
 
-            Complex z = new Complex();
+            Complex x = new Complex();
+            x.Re = 5;
+            x.Im = 7;
 
-            z.Re = 5;
-            z.Im = 7;
+            Complex y = new Complex();
+            y.Re = 7;
+            y.Im = 10;
 
-            Console.WriteLine("z = {0}+i{1}", z.Re, z.Im);
-            Console.WriteLine();
+            var z = x.Add(y);
+            var z1 = x + y;
+            var z2 = x - y;
+
+            x.Print();
+            y.Print();
             z.Print();
+            z1.Print();
+            z2.Print();
 
             Console.WriteLine("Нажмите Enter для выхода...");
             Console.ReadLine(); // В конце для предотвращения закрытия окна
@@ -33,7 +41,34 @@ namespace OOPFirstApp
 
         public void Print()
         {
-            Console.WriteLine("z = {0}+i{1}", Re, Im);
+            Console.WriteLine("{0}+i{1}", Re, Im);
+        }
+
+        public Complex Add(Complex y)
+        {
+            var z = new Complex();
+            z.Re = Re + y.Re;
+            z.Im = Im + y.Im;
+
+            return z;
+        }
+
+        public static Complex operator +(Complex x, Complex y)
+        {
+            var z = new Complex();
+            z.Re = x.Re + y.Re;
+            z.Im = x.Im + y.Im;
+
+            return z;
+        }
+
+        public static Complex operator -(Complex x, Complex y)
+        {
+            var z = new Complex();
+            z.Re = x.Re - y.Re;
+            z.Im = x.Im - y.Im;
+
+            return z;
         }
     }
 }
