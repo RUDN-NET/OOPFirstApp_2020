4de8a7f270a9a3adff6180f6fd877a338c5f1378
Infarh <shmachilin@yandex.ru>
2020-09-22 09:31:01 +0300

    Заполнение массива случайными числами

--- OOPFirstApp/Program.cs
+++ OOPFirstApp/Program.cs
@@ -6,7 +6,30 @@ namespace OOPFirstApp
     {
         static void Main(string[] args)
         {
+            const int array_length = 50;
 
+            var data = new int[array_length];
+
+            var rnd = new Random();
+            for (var i = 0; i < array_length; i++)
+            {
+                //var x_random = (rnd.NextDouble() - 0.5) * 2 * 5;
+                //var int_random = rnd.Next();
+                //var int2_random = rnd.Next(51); // 0..50
+                //data[rnd.Next(data.Length)] = 5; // Запись значения в случайную ячейку массива
+                //var random_data = data[rnd.Next(array_length)]; // Получить значение из случайной ячейки
+                //var int3_random = rnd.Next(-50,51); // -50..50
+
+                data[i] = rnd.Next(0, 101);
+            }
+
+            Console.WriteLine("Исходный массив");
+            foreach (var value in data)
+                Console.WriteLine(value);
+            Console.WriteLine();
+
+            Console.WriteLine("Нажмите Enter для выхода...");
+            Console.ReadLine(); // В конце для предотвращения закрытия окна
         }
     }
 }
