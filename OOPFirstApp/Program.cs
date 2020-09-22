fdd4654c2864c8880182ace287683455b11f4abe
Infarh <shmachilin@yandex.ru>
2020-09-22 11:22:45 +0300

    Создан класс-наследник для ружья

--- OOPFirstApp/Program.cs
+++ OOPFirstApp/Program.cs
@@ -14,7 +14,7 @@ namespace OOPFirstApp
             //ComplexTests.Start();
 
             Gun gun1 = new Gun();
-            Gun gun2 = new Gun(5, '#');
+            Gun2 gun2 = new Gun2(5, '#');
             //gun2.ShotSymbol = '*';
 
             //gun2.SetAmmoCount(-5);
@@ -58,8 +58,8 @@ namespace OOPFirstApp
 
     class Gun
     {
-        private int _AmmoCount;
-        private char _ShotSymbol = '|';
+        protected int _AmmoCount;
+        protected char _ShotSymbol = '|';
 
         public int GetAmmoCount()
         {
@@ -135,6 +135,34 @@ namespace OOPFirstApp
         public void LoadAmmo(int count)
         {
             _AmmoCount += count;
+        }
+    }
+
+    class Gun2 : Gun
+    {
+        public Gun2(int count, char symbol)
+            : base(count, symbol) // Вызываем конструктор базового класса и передаём ему нужные параметры
+        {
+            //_AmmoCount = count;
+            //_ShotSymbol = symbol;
+            Console.WriteLine("\tсоздан второй ствол для ружья");
+        }
+
+        public void Shot()
+        {
+            for (var i = 0; i < 2; i++)
+                if (_AmmoCount > 0)
+                {
+                    Console.Write(_ShotSymbol);
+                    _AmmoCount--;
+                }
+                else
+                {
+                    Console.WriteLine("Патроны закончились");
+                    return;
+                }
+
+            Console.WriteLine();
         }
     }
 }
