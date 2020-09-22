79897480cb7190859235da6b982f6f60bfc56545
Infarh <shmachilin@yandex.ru>
2020-09-22 11:13:16 +0300

    Свойства для комплексных чисел

--- OOPFirstApp/Complex.cs
+++ OOPFirstApp/Complex.cs
@@ -4,38 +4,73 @@ namespace OOPFirstApp
 {
     class Complex
     {
-        public double Re;
-        public double Im;
+        private double _Re;
+        private double _Im;
+
+        public double Re
+        {
+            get
+            {
+                return _Re;
+            }
+            set
+            {
+                _Re = value;
+            }
+        }
+
+        public double ReadOnlyRe => _Re;
+
+        public double Im
+        {
+            get => _Im;
+            set => _Im = value;
+        }
+
+        public double Abs // Свойство только для чтения - блок set отсутствует
+        {
+            get
+            {
+                return Math.Sqrt(_Re * _Re + _Im * _Im);
+            }
+        }
+
+        //public double Arg
+        //{
+        //    get => Math.Atan2(_Im, _Re);
+        //}
+
+        public double Arg => Math.Atan2(_Im, _Re); // Сокращённая запись свойства только для чтения
 
         public void Print()
         {
-            Console.WriteLine("{0}+i{1}", Re, Im);
+            Console.WriteLine("{0}+i{1}", _Re, _Im);
         }
 
         public Complex Add(Complex y)
         {
             var z = new Complex();
-            z.Re = Re + y.Re;
-            z.Im = Im + y.Im;
+            z._Re = _Re + y._Re;
+            z._Im = _Im + y._Im;
 
             return z;
         }
 
         public double GetAbs()
         {
-            return Math.Sqrt(Re * Re + Im * Im);
+            return Math.Sqrt(_Re * _Re + _Im * _Im);
         }
 
         public double GetArg()
         {
-            return Math.Atan2(Im, Re);
+            return Math.Atan2(_Im, _Re);
         }
 
         public static Complex operator +(Complex x, Complex y)
         {
             var z = new Complex();
-            z.Re = x.Re + y.Re;
-            z.Im = x.Im + y.Im;
+            z._Re = x._Re + y._Re;
+            z._Im = x._Im + y._Im;
 
             return z;
         }
@@ -43,8 +78,8 @@ namespace OOPFirstApp
         public static Complex operator -(Complex x, Complex y)
         {
             var z = new Complex();
-            z.Re = x.Re - y.Re;
-            z.Im = x.Im - y.Im;
+            z._Re = x._Re - y._Re;
+            z._Im = x._Im - y._Im;
 
             return z;
         }
